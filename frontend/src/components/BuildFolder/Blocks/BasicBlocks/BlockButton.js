b49d05335c37b0f93832a3c45701caeb15553807
andybrvt <andybrvt@gmail.com>
2022-02-01 15:20:14 -0700

    try new design

--- frontend/src/components/BuildFolder/Blocks/BasicBlocks/BlockButton.js
+++ frontend/src/components/BuildFolder/Blocks/BasicBlocks/BlockButton.js
@@ -1,3 +1,40 @@
+// NOTES FOR FUTURE: if you want to add your own functions
+// to a new block, you have to create your own type
+
 export const basicBlocks = [
-  {label: 'Label text 3', content: '<div>Content</div>'},
+  {
+    label: 'Button',
+    attributes: {
+        class: "gjs-fonts gjs-f-button"
+    },
+    content: "<button class='btn btn-success'>Button</button>",
+    id: "myButtonId"
+  },
+  {
+    media:
+    `
+      <div class="header">Content</div>
+
+      <style>
+        .header{
+          color: red
+        }
+      </style>
+
+    `,
+    content:
+    `
+      <div class = "">
+        <div class="header">Content</div>
+      </div>
+
+      <style>
+        .header{
+          color: red
+        }
+      </style>
+
+    `
+
+  },
 ]

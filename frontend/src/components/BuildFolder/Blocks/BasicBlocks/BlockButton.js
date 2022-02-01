dcb78e2a24e44f779f89478553efe905e9e38408
pinghsu520 <pinghsu520@gmail.com>
2022-02-01 16:27:47 -0700

    bawsic shapes

--- frontend/src/components/BuildFolder/Blocks/BasicBlocks/BlockButton.js
+++ frontend/src/components/BuildFolder/Blocks/BasicBlocks/BlockButton.js
@@ -37,4 +37,31 @@ export const basicBlocks = [
     `
 
   },
+  {
+    id: 'triangle',
+    label: 'Triangle ',
+    // Select the component once it's dropped
+    select: true,
+    // You can pass components as a JSON instead of a simple HTML string,
+      // in this case we also use a defined component type `image`
+    content: `<span  style="border-left: 40px solid transparent;
+border-right: 40px solid transparent;
+border-bottom: 80px solid lightblue; "> </span> `,
+    // This triggers `active` event on dropped components and the `image`
+    // reacts by opening the AssetManager
+    activate: true,
+  },
+
+{
+  id: 'circle',
+  label: '<span  style="background-color:powderblue; border-radius:50%; width:100px; height:50px;  "> tt</span> ',
+  // Select the component once it's dropped
+  select: true,
+  // You can pass components as a JSON instead of a simple HTML string,
+    // in this case we also use a defined component type `image`
+  content: `<span  style="background-color:powderblue; border-radius:50%; width:50px; height:50px;  "> tt</span> `,
+  // This triggers `active` event on dropped components and the `image`
+  // reacts by opening the AssetManager
+  activate: true,
+},
 ]

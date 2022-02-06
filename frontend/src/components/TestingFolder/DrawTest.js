7660ce0056706eb9d736ba5e28b78a6b0e3ea17d
andybrvt <andybrvt@gmail.com>
2022-02-06 16:13:01 -0700

    creating lines

--- frontend/src/components/TestingFolder/DrawTest.js
+++ frontend/src/components/TestingFolder/DrawTest.js
@@ -10,86 +10,98 @@ var hitOptions = {
 const DrawTest = () => {
 
   // Paper.settings.handleSize = 10;
-  let myPath = new Paper.Path();
-  myPath.onMouseEnter = (event) => {
-    console.log('enter')
-    myPath.fullySelected = true
-  }
-  myPath.onMouseLeave = (event) => {
-    console.log('leave')
-    myPath.fullySelected = false
-  }
-
-  let myCircle = new Paper.Path.Circle({
-    center: Paper.view.center,
-    radius: 30,
-    strokeColor: 'black',
-  });
-
-
   var tool = new Paper.Tool();
 
 
-  var segment, path;
-  var movePath = false
+  const createNewPath = () => {
+    var newPath = new Paper.Path();
+    newPath.strokeColor = "black";
+    newPath.strokeWidth = 5;
+
+    return newPath
+
+  }
+
+  var path;
+  // var segment, path;
+  // var movePath = false
   Paper.view.onMouseDown = (event) => {
-    segment = path = null;
-    console.log('mouse down here')
-    // check what this is
-    var hit = Paper.project.hitTest(event.point, hitOptions);
-
-
-    if(hit){
-      if(hit.location){
-          var curve = hit.location.curve;
-      }
-
-      if(hit.type === "segment"){
-        segment = hit.segment
-      } else if(hit.type === "stroke"){
-        // var location = hit.location;
-        // segment = path.insert(location.index + 1, event.point);
-        // path.smooth()
-      }
-      console.log(hit.item, hit.type)
-    }
-
-    else {
-      myPath.strokeColor = "black";
-      myPath.strokeWidth = 3;
+    if(path){
 
     }
+
+    if(!path){
+      var newPath = createNewPath();
+      path = newPath;
+      path.add(event.point)
+
+    } else {
+      console.log(path)
+      path.add(event.point)
+      path.fullySelected= true;
+
+    }
+
+
+
+    // segment = path = null;
+    // console.log('mouse down here')
+    // // check what this is
+    // var hit = Paper.project.hitTest(event.point, hitOptions);
+    //
+    //
+    // if(hit){
+    //   console.log(hit)
+    //   if(hit.location){
+    //       var curve = hit.location.curve;
+    //       curve.fullySelected = true;
+    //   }
+    //
+    //   if(hit.type === "segment"){
+    //     segment = hit.segment
+    //   } else if(hit.type === "stroke"){
+    //     // var location = hit.location;
+    //     // segment = path.insert(location.index + 1, event.point);
+    //     // path.smooth()
+    //   }
+    //   console.log(hit.item, hit.type)
+    // }
+    //
+    // else {
+    //   myPath.strokeColor = "black";
+    //   myPath.strokeWidth = 3;
+    //
+    // }
 
   };
 
   Paper.view.onMouseDrag = (event) =>{
-    if(segment){
-      console.log(segment.point)
-      console.log(event.delta)
-      segment.point.set(event.point)
-      myPath.smooth()
-    } else if(path){
-      // path.position += event.delta;
-    } else {
-        myPath.add(event.point);
-    }
+    // if(segment){
+    //   console.log(segment.point)
+    //   console.log(event.delta)
+    //   segment.point.set(event.point)
+    //   myPath.smooth()
+    // } else if(path){
+    //   // path.position += event.delta;
+    // } else {
+    //     myPath.add(event.point);
+    // }
 
   }
 
   Paper.view.onMouseUp = (event) => {
-    console.log('on lift pen')
-    myPath.simplify()
-    myPath.fullySelected = true;
+    // path.add(event.point)
+    // path.fullySelected = true;
   }
 
   Paper.view.onMouseMove = (event) => {
-    console.log('move mouse')
-    var hit = Paper.project.hitTest(event.point)
-
-    if(hit && hit.location){
-      var curve = hit.location.curve;
-      curve.fullySelected = true
-    }
+    // console.log('move mouse')
+    // var hit = Paper.project.hitTest(event.point)
+    //
+    // if(hit && hit.location){
+    //   var curve = hit.location.curve;
+    //   curve.fullySelected = true
+    // }
 
 
     // var radius = 1;

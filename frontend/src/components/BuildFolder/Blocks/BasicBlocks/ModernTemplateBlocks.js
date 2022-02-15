a769073533df8dd83de2a9f7819a9fa6b78c7201
andybrvt <andybrvt@gmail.com>
2022-02-14 21:19:08 -0700

    style changes

--- frontend/src/components/BuildFolder/Blocks/BasicBlocks/ModernTemplateBlocks.js
+++ frontend/src/components/BuildFolder/Blocks/BasicBlocks/ModernTemplateBlocks.js
@@ -6,7 +6,7 @@ const rowStyle  = `
     justify-content: flex-start;
     align-items: stretch;
     flex-wrap: nowrap;
-    padding: 10px;
+    color: white;
   }
 `
 const rowStyleMedia = `
@@ -23,8 +23,7 @@ const colStyle = `
 .colStyleM{
   position: relative;
   min-height: 100px;
-  max-height: 80vh;
-  height: 80vh;
+  height: 100vh;
   flex-grow: 1;
   flex-basis: 100%;
   overflow:hidden;
@@ -50,24 +49,50 @@ export const ModernTemplateBlocks = [
     media: `<div>Stuff here</div>`,
     content: `
       <style>
+      @font-face {
+         font-family: myFirstFont;
+         src: url(sansation_light.woff);
+      }
+
+      * {
+         font-family: myFirstFont;
+      }
         ${rowStyle}
         ${colStyle}
         .gradientBackground{
-          padding:25px;
           text-align: center;
           color: white;
-          min-height: 500px;
+          height: 100%;
           font: caption;
-          background-image: linear-gradient(90deg, #00FFFF 1%, #F5F5DC 99%)
+          background-image: linear-gradient(90deg, #8a2be2 1%, #ba55d3 99%)
+        }
+        .mHeaderText{
+          position: absolute;
+          font-size: 70px;
+          width: 30%;
+          left: 12%;
+          top: 20%;
+
+
+        }
+        .mBodyText{
+          position: absolute;
+          width: 30%;
+          left: 12%;
+          top: 45%;
         }
       </style>
       <div class="rowStyleM">
         <div class="colStyleM">
-
-        <div
-          class ='gradientBackground'
-        ></div>
+          <div class ='gradientBackground'></div>
         </div>
+        <h1 class ="mHeaderText">
+          Brand New Protocol Here
+        </h1>
+        <div class = "mBodyText">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </div>
+
       </div>
     `
   }

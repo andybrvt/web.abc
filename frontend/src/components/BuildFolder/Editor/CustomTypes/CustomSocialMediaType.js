2f7dac4a46bc2339cd683143fbc4af4c3f0bc85a
andybrvt <andybrvt@gmail.com>
2022-03-27 19:13:27 -0700

    mgot the icon css for social media

--- frontend/src/components/BuildFolder/Editor/CustomTypes/CustomSocialMediaType.js
+++ frontend/src/components/BuildFolder/Editor/CustomTypes/CustomSocialMediaType.js
@@ -16,11 +16,66 @@ export const CustomSocialMediaFooter = editor => {
       tagName: 'SocialMediaFooter',
       resizable: 'true',
       defaults: {
+
+        attributes: {class: 'social-media-footer-block'},
+        styles: `
+          .social-media-footer-block{
+            position: relative;
+          }
+          .social-media-footer-container{
+            display: flex;
+            flex-direction: row;
+            position: relative;
+            left: 50%;
+            width: 60%;
+            transform: translateX(-50%);
+            padding: 25px 0px;
+          }
+          .socialMediaIcon{
+            flex: 1;
+            margin: 2px;
+            padding: 10px;
+            font-size: 25px;
+            text-align:center;
+          }
+          .facebookIcon{
+            color:#1778f2;
+          }
+
+          .instagramIcon{
+            color: purple
+          }
+          .twitterIcon{
+            color: #00acee;
+          }
+          .discordIcon{
+            color: #5865F2
+          }
+          .linkedInIcon{
+            color: #0072b1;
+          }
+
+        `,
         components: model => {
 
           return(
-            <div>
-              <i class="fab fa-facebook"></i>
+            <div class = "social-media-footer-container">
+              <div class = "socialMediaIcon facebookIcon">
+                <i class="fab fa-facebook"></i>
+              </div>
+              <div class = "socialMediaIcon instagramIcon">
+                <i class="fab fa-instagram"></i>
+              </div>
+              <div class = "socialMediaIcon twitterIcon">
+                <i class="fab fa-twitter"></i>
+              </div>
+              <div class = 'socialMediaIcon discordIcon'>
+                <i class="fab fa-discord"></i>
+              </div>
+              <div class = 'socialMediaIcon linkedInIcon'>
+                <i class="fab fa-linkedin"></i>
+              </div>
+
             </div>
           )
         }

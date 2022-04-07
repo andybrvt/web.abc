c65b031144eb87a9eec0aa2ee73a53af5ac8a741
andybrvt <andybrvt@gmail.com>
2022-04-07 01:03:10 -0700

    being able to add in an extra step with a enter button

--- frontend/src/components/BuildFolder/NFT/NFTRoadMapModal.js
+++ frontend/src/components/BuildFolder/NFT/NFTRoadMapModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   Modal,
   ModalOverlay,
@@ -9,6 +9,7 @@ import {
   ModalCloseButton,
   useDisclosure,
   Button,
+  Input
 } from '@chakra-ui/react';
 import { useMoralis, useMoralisWeb3Api, useMoralisCloudFunction } from "react-moralis";
 import './PickNFTModal.css'
@@ -17,13 +18,14 @@ import ImagePickerCustom from '../../UsefulComponents/ImagePicker';
 import { useEthers } from "@usedapp/core";
 import { Checkbox } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom';
-
+import './NFTRoadMapModal.css';
 
 export const NFTRoadMapModal  = (props) => {
 
   const { web3 } = useMoralis()
   const Web3Api = useMoralisWeb3Api()
   const [editorMain, setEditorMain] = useState(null)
+  const [inputs, setInputs] = useState({1:"",})
 
   const {websiteId, buildType} = useParams()
 
@@ -41,6 +43,30 @@ export const NFTRoadMapModal  = (props) => {
     props.onClose()
   }
 
+  const enterKeyPress = (event) => {
+
+    console.log(event.key)
+    if(event.key === "Enter"){
+
+      const container = document.getElementById("roadMapInputContainer")
+      console.log(container)
+      const input =  document.createElement("input");
+      input.className = "form-control";
+      input.type = "text";
+      input.placeholder = "step";
+
+      const inputContainer = document.createElement("div");
+      inputContainer.className = "inputContainer";
+
+      inputContainer.append(input)
+      container.append(inputContainer)
+
+
+
+
+    }
+  }
+
 
 
   return(
@@ -49,14 +75,20 @@ export const NFTRoadMapModal  = (props) => {
       onClose={onCloseModal}
       isOpen={props.isOpen}
       scrollBehavior={props.scrollBehavior}
-      size = {'6xl'}
     >
       <ModalOverlay />
       <ModalContent>
 
         <ModalCloseButton />
         <ModalBody>
-          Hi
+            <div>NFT Project RoadMap</div>
+
+            <div id ="roadMapInputContainer">
+              <input
+                onKeyDown={enterKeyPress}
+                type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+
+            </div>
 
         </ModalBody>
 

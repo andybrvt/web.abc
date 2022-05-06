2bdaefe1f3dd1e2c346984ed40f00f1347c09139
andybrvt <andybrvt@gmail.com>
2022-05-05 21:56:44 -0700

    DELETED IFRAME CHECK HERE

--- frontend/src/components/BuildFolder/Editor/OfficialWebsite/OfficialPage.js
+++ frontend/src/components/BuildFolder/Editor/OfficialWebsite/OfficialPage.js
@@ -22,49 +22,70 @@ export const OfficialPage = props => {
       setHtml(res.data.html)
       setCss(res.data.css)
 
-      setSrcDoc(
-        `
-        <html>
-          <head>
-            <script src="https://kit.fontawesome.com/2638379ee9.js" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
-            <script src="https://unpkg.com/moralis/dist/moralis.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-          </head>
-          <body>
-
-          ${res.data.html}
-          </body>
-          <style>${res.data.css}</style>
-          <script>${res.data.js}</script>
-          <script>
-            $(function () {
-              $('[data-toggle="tooltip"]').tooltip()
-            })
-          </script>
-          <script>
-            function copyAddress() {
-              /* Get the text field */
-              var copyText = document.getElementsByClassName("copy-to-clipboard-address-button")[0];
-
-              console.log(copyText.getAttribute('value'))
-
-              /* Copy the text inside the text field */
-              navigator.clipboard.writeText(copyText.getAttribute('value'));
-
-            }
-          </script>
-        <html>
-
-        `
-      )
-
     })
 
   }, [])
+
+
+  useEffect(() => {
+
+    if(account !== undefined){
+      const connectToWalletBtns = document.getElementsByClassName("connect-to-wallet")
+      if(connectToWalletBtns !== null){
+        for(let i =0; i<connectToWalletBtns.length; i++){
+            connectToWalletBtns[i].innerHTML = account
+            connectToWalletBtns[i].addEventListener("click", function(){
+              deactivate()
+              console.log('why')
+            })
+          }
+        }
+
+    } else {
+      const connectToWalletBtns = document.getElementsByClassName("connect-to-wallet")
+
+      if(connectToWalletBtns !== null){
+
+        if(connectToWalletBtns.length > 0){
+            for(let i =0; i<connectToWalletBtns.length; i++){
+
+              connectToWalletBtns[i].innerHTML = "Connect to Wallet"
+
+                connectToWalletBtns[i].addEventListener("click", function(){
+                activateBrowserWallet()
+
+              })
+            }
+          }
+
+      }
+
+    }
+
+  },[account])
+
+
+  useEffect(() => {
+
+
+    const connectToWalletBtns = document.getElementsByClassName("connect-to-wallet")
+
+    if(connectToWalletBtns !== null){
+
+      if(connectToWalletBtns.length > 0){
+          for(let i =0; i<connectToWalletBtns.length; i++){
+            console.log(connectToWalletBtns[i], 'anything here')
+              connectToWalletBtns[i].addEventListener("click", function(){
+              activateBrowserWallet()
+
+            })
+          }
+        }
+
+    }
+
+
+  }, [html])
 
   return(
     <div>
@@ -73,18 +94,7 @@ export const OfficialPage = props => {
       <style>
         {css}
       </style>
-      {/*
 
-        <iframe
-          srcDoc = {srcDoc}
-          // sandbox= "allow-scripts" // just so that you cant access other codes out side
-          title = "output"
-          frameBorder = "0"
-          width = "100%"
-          height = "100%"
-           />
-
-        */}
 
     </div>
   )
--- frontend/src/components/BuildFolder/Editor/PreviewPage.js
+++ frontend/src/components/BuildFolder/Editor/PreviewPage.js
@@ -32,45 +32,7 @@ export const PreviewPage = props => {
         setCss(res.data.css)
         eval(res.data.js)
 
-        setSrcDoc(
-          `
-          <html>
-            <head>
-              <script src="https://kit.fontawesome.com/2638379ee9.js" crossorigin="anonymous"></script>
-              <script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
-              <script src="https://unpkg.com/moralis/dist/moralis.js"></script>
-              <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-            </head>
-            <body>
-
-            ${res.data.html}
-            </body>
-            <style>${res.data.css}</style>
-            <script>${res.data.js}</script>
-            <script>
-              $(function () {
-                $('[data-toggle="tooltip"]').tooltip()
-              })
-            </script>
-            <script>
-              function copyAddress() {
-                /* Get the text field */
-                var copyText = document.getElementsByClassName("copy-to-clipboard-address-button")[0];
-
-                console.log(copyText.getAttribute('value'))
-
-                /* Copy the text inside the text field */
-                navigator.clipboard.writeText(copyText.getAttribute('value'));
-
-              }
-            </script>
-          <html>
-
-          `
-        )
+    
 
       })
 
@@ -180,19 +142,6 @@ export const PreviewPage = props => {
 
       </div>
 
-
-      {/*
-        <iframe
-          srcDoc = {srcDoc}
-          // sandbox= "allow-scripts" // just so that you cant access other codes out side
-          title = "output"
-          frameBorder = "0"
-          width = "100%"
-          height = "100%"
-           />
-
-
-        */}
 
     </div>
   )

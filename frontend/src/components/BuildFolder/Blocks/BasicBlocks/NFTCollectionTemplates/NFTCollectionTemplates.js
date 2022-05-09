00cd2e1276665c0503ca61742947a73f474a71be
pinghsu520 <pinghsu520@gmail.com>
2022-05-09 16:52:59 -0700

    icon move

--- frontend/src/components/BuildFolder/Blocks/BasicBlocks/NFTCollectionTemplates/NFTCollectionTemplates.js
+++ frontend/src/components/BuildFolder/Blocks/BasicBlocks/NFTCollectionTemplates/NFTCollectionTemplates.js
@@ -99,7 +99,7 @@ const tempHeight = `
 export const NFTCollectionTemplates = [
   {
     id: 'mTemplate1',
-    category: 'NFT Templates',
+    category: 'NFT Templatessss',
     media: `
     <style>
       ${headerStyle}
@@ -110,104 +110,15 @@ export const NFTCollectionTemplates = [
       ${headerItemMedia}
       ${mediaButton}
       ${background}
-      .gradientBackground1{
-        // background-image: linear-gradient(90deg, #4B0082 1%, #ba55d3 99%)
-      }
-      .discordIcon{
-        width:200px;
-        height:200px;
-      }
-      .mHeaderText{
-        color: black;
-        position: absolute;
-        font-size: 100px;
-        width: 30%;
-        left: 12%;
-        top: 20%;
-        font-size: 15px;
-        font-family: Lato;
-        text-align:left;}
-      .mBodyText{
-        text-align: left;
-        color:black;
-        position: absolute;
-        width: 30%;
-        left: 12%;
-        top: 40%;
-        font-size: 5px;
-        font-family: Lato;}
-      .mButton1{
-          background-image: linear-gradient(90deg, #483D8B 1%,#8B008B 99%);
-          left: 12%;
-          top: 75%;}
-      .mButton2{
-          background: black;
-          left: 27%;
-          top: 75%;}
-      .monkeyImage{
-        border-radius: 20px;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-        position: absolute;
-        right: 5%;
-        top: 2.5%;
-        width:100px;
-        height:100px;
-      }
-
-      
+     
     </style>
 
       <div data-gjs-dmode="absolute" class="rowStyleM">
-        <div class="colStyleM">
-          <div class ='gradientBackground gradientBackground1'/>
+        <div>test</div>
         </div>
 
-        <div class="header">
-          <a class="logo">
-            <svg width="25" height="25" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_1_196)">
-            <rect width="1024" height="1024" fill="#531DAB"/>
-            <path d="M543.395 693L544.595 711C544.595 717.4 534.995 720.6 515.795 720.6C482.195 720.6 465.395 714.2 465.395 701.4C465.395 688.6 474.195 661 491.795 618.6C509.795 576.2 518.795 549.8 518.795 539.4C518.795 518.2 505.795 507.6 479.795 507.6L470.195 508.2C465.395 508.2 462.995 506.4 462.995 502.8C462.995 495.2 465.795 487.6 471.395 480C477.395 472 484.795 468 493.595 468C529.995 468 562.595 457.2 591.395 435.6C620.195 414 634.595 391.2 634.595 367.2C634.595 350 629.195 338 618.395 331.2C607.995 324.4 593.595 321 575.195 321C556.795 321 542.795 326.6 533.195 337.8C509.595 367.4 471.595 429.4 419.195 523.8C367.195 617.8 337.995 675.6 331.595 697.2C327.995 707.2 318.595 714.6 303.395 719.4C288.595 724.2 273.995 726.6 259.595 726.6C245.595 726.6 238.595 723.8 238.595 718.2V717C238.595 712.6 252.395 687 279.995 640.2C307.595 593.4 337.995 541.4 371.195 484.2C404.795 426.6 426.395 386.8 435.995 364.8C445.995 342.8 450.995 324.2 450.995 309V303C450.995 292.2 481.395 286.8 542.195 286.8C607.395 286.8 650.395 293.4 671.195 306.6C679.995 312.2 687.395 320.4 693.395 331.2C699.795 342 702.995 355.8 702.995 372.6C702.995 389.4 696.195 406.4 682.595 423.6C668.995 440.8 654.395 453.6 638.795 462C623.595 470 608.395 475.8 593.195 479.4C577.995 483 566.595 486 558.995 488.4C551.395 490.8 547.595 494.6 547.595 499.8C547.595 503 550.395 507 555.995 511.8C561.995 516.2 567.995 521.6 573.995 528C579.995 534.4 582.995 541.2 582.995 548.4C582.995 555.6 576.395 576.4 563.195 610.8C549.995 644.8 543.395 672.2 543.395 693Z" fill="white"/>
-            </g>
-            <defs>
-            <clipPath id="clip0_1_196">
-            <rect width="1024" height="1024" fill="white"/>
-            </clipPath>
-            </defs>
-            </svg>
 
-          </a>
-          <div class="menuM">
-            <a class="headerItem">
-              Home
-            </a>
-            <a class="headerItem">
-              About
-            </a>
-            <a class="headerItem">
-              Our Team
-            </a>
-            <a class="headerItem">
-              Funding
-            </a>
-          </div>
-        </div>
-
-        <h1 class ="mHeaderText">
-          Brand New Protocol
-        </h1>
-        <div class = "mBodyText">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        </div>
-        <div data-gjs-type="button" class='mButton mButton1'>
-          Go to Page
-        </div>
-        <div data-gjs-type="button1" class='mButton mButton2'>
-          Buy Now
-        </div>
-        <img class ="monkeyImage" src = "https://images.unsplash.com/photo-1605792657660-596af9009e82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1102&q=80"/>
-        
-        </div>
+    </div>
     `,
     content: `
       <style>
--- frontend/src/components/BuildFolder/Blocks/Web3Blocks/NFTCollectionBlocks.js
+++ frontend/src/components/BuildFolder/Blocks/Web3Blocks/NFTCollectionBlocks.js
@@ -1,7 +1,530 @@
 import image25 from '../../../../images/image25.png';
+import {
+  rowStyle,
+  colStyle,
+  normalButton,
+  rowStyleMedia,
+  colStyleMedia
+} from '../BasicBlocks/GeneralBlockCss';
+import astro from '../BasicBlocks/NFTCollectionTemplates/astro.svg';
+import coding from '../BasicBlocks/NFTCollectionTemplates/coding.svg';
+import apartment from '../BasicBlocks/NFTCollectionTemplates/apartment.svg';
 
+import twitter from '../BasicBlocks/NFTCollectionTemplates/twitter.png';
+import opensea from '../BasicBlocks/NFTCollectionTemplates/opensea.png';
+import discord from '../BasicBlocks/NFTCollectionTemplates/discord.png';
+import space1 from '../BasicBlocks/NFTCollectionTemplates/space1.svg';
+import space2 from '../BasicBlocks/NFTCollectionTemplates/space2.svg';
+import space3 from '../BasicBlocks/NFTCollectionTemplates/space3.svg';
+const menu = `
+  .menuM{
+    display: flex;
+    flex-direction: row;
+    margin-left: auto;
+    margin-right: 5%;
+  }`
+const logo = `
+  .logo{
+    top: 100px;
+    width: 100px;
+    margin-left: 7%;
+    font-weight: bold;
+    text-align:center;
+  }`
+const headerStyle = `
+  .header{
+    position: absolute;
+    top: 0;
+    width: 100%;
+    display:flex;
+  }`
+const headerItem = `
+  .headerItem{
+    text-align:center;
+    justify-content: center;
+    margin-right:35px;
+    font-size: 20px;
+    font-family: Lato;
+  }`
+const headerItemMedia = `
+  .headerItem{
+    text-align: center;
+    justify-content:center;
+    padding: 7px;
+    font-size: 20px;
+  }`
+  const mediaButton = `
+  .mButton{
+    position: absolute;
+    display: inline-flex;
+    align-items: center;
+    padding: 0 10px;
+    height: 20px;
+    font-size: 5px;
+    border-radius: 7px;
+    font-family: Monospace;
+    color:white;
+  }`
+
+
+// FOR BACKGROUND
+const background = `
+.gradientBackground{
+  text-align: center;
+  color: white;
+  height: 100%;
+}`
+
+const tempHeight = `
+  .tempHeight{
+    height: 90vh;
+  }
+`
 
 export const NFTCollectionBlocks = [
+  {
+    id: 'mTemplate1',
+    category: 'NFT Templatessss',
+    media: `
+    <style>
+      ${headerStyle}
+      ${rowStyleMedia}
+      ${colStyleMedia}
+      ${menu}
+      ${logo}
+      ${headerItemMedia}
+      ${mediaButton}
+      ${background}
+     
+    </style>
+
+      <div data-gjs-dmode="absolute" class="rowStyleM">
+        <div>test</div>
+        </div>
+
+
+    </div>
+    `,
+    content: `
+      <style>
+        ${rowStyle}
+        ${colStyle}
+        ${background}
+        ${headerStyle}
+        ${menu}
+        ${logo}
+        ${headerItem}
+        ${normalButton}
+        ${tempHeight}
+        .gradientBackground1{
+          // background-image: linear-gradient(90deg, #4B0082 1%, #ba55d3 99%)
+        }
+        .mHeaderText{
+          position: absolute;
+          font-size: 70px;
+          width: 30%;
+          left: 12%;
+          top: 20%;
+          font-family: Lato;}
+        .mBodyText{
+          position: absolute;
+          width: 25%;
+          left: 12%;
+          top: 45%;
+          font-family: Lato;
+          font-size:20px;
+        }
+        .mButton1{
+            background-image: linear-gradient(90deg, #483D8B 1%,#8B008B 99%);
+            left: 12%;
+            top: 70%;}
+        .mButton2{
+            background: black;
+            left: 25%;
+            top: 70%;}
+        .mImage{
+          border-radius: 25px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+          position: absolute;
+          right: 5%;
+          top: 15%;
+          width: 50%;}
+
+        .teamTitle{
+          font-size:35px;
+          font-family: Lato;
+          margin-bottom:45px;
+          margin-top:45px;
+
+        }
+        
+
+        .teamSection {    
+          position: relative;
+          left: 12%;
+          top: 300%;
+          width: 90%;
+          height:100%;
+        }
+        .teamContainer {
+          display:flex;
+          flex-direction:row;
+        }
+        .teamFirstPerson {
+       
+         display:flex;
+         flex-direction:column;
+        }
+        .teamSecondPerson{
+  
+          display:flex;
+         flex-direction:column;
+        }
+        .tripleComparisonHeader{
+          font-size:45px;
+          margin-top:10px;
+          margin-bottom:20px;
+          font-family: Lato;
+          font-weight:800;
+
+        }
+
+        .tripleComparisonSubHeader{
+          font-size:30px;
+          margin-top:10px;
+          margin-bottom:10px;
+          font-family: Lato;
+        }
+
+        .tripleComparisonContainer {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin-bottom:20px;
+
+        }
+
+        .tripleComparisonSection {    
+          position: relative;
+          left: 12%;
+          top: 200%;
+          width: 85%;
+          height:50%;
+        }
+        
+        .nftCollectionTitleSection {    
+          position: relative;
+          left: 12%;
+          top: 65%;
+          width: 85%;
+          height:25%;
+        }
+
+        .nftCollectionTitlePosition {    
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+          margin-bottom:20px;
+        }
+
+
+        .NFTSpecPosition {    
+          position: relative;
+          right: 2.5%;
+          top: 125%;
+          width: 50%;
+          height:50%;
+        }
+
+        .NFTSpecContainer {    
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin-bottom:20px;
+        }
+
+        .NFTSpecTitle{
+          font-size:45px;
+          margin-top:10px;
+          margin-bottom:10px;
+          font-weight:800;
+          font-family: Lato;
+        }
+
+        .MFTSpecContent{
+          width:80%;
+          font-size:22px;
+          margin-top:10px;
+          margin-bottom:10px;
+  
+          font-family: Lato;
+        }
+
+        .testFirstPersonImage{
+          border-radius: 12px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+          width:350px;
+          height:350px;
+          
+        }
+
+        .card {
+          /* Add shadows to create the "card" effect */
+          box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+          transition: 0.3s;
+          border-radius: 12px;
+          margin-right: 20px;
+        }
+
+        .infoSection{
+          flex:1;
+          
+          
+        }
+
+        .itemcenter {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .infoSectionTitle{
+          font-size:37px;
+          margin-top:10px;
+          margin-bottom:10px;
+          font-weight:800;
+          font-family: Lato;
+        }
+
+        .infoSectionDesc{
+          width:80%;
+          font-size:22px;
+          margin-top:10px;
+          margin-bottom:10px;
+  
+          font-family: Lato;
+        }
+
+
+        .teamPersonName{
+          font-size:27.5px;
+          font-weight:bold;
+          font-family: Lato;
+        }
+        
+        .teamPersonRole{
+          font-size:20px;
+          font-family: Lato;
+        }
+        
+        /* On mouse-over, add a deeper shadow */
+        .card:hover {
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        }
+        d
+        /* Add some padding inside the card container */
+        .container {
+          padding: 25px;
+        }
+
+        .monkeyImage{
+          border-radius: 20px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+          position: relative;
+          right: 50%;
+          top: 10%;
+          width:200px;
+          height:200px;
+        }
+        
+        .space1Image{
+          position: relative;
+          right: 50%;
+          top: 110%;
+          width:600px;
+          height:600px;
+        }
+        .space2Image{
+          position: relative;
+          right: 50%;
+          top: 20%;
+          width:350px;
+          height:350px;
+        }
+        .space3Image{
+          position: relative;
+          right: 20%;
+          top: 20%;
+          width:350px;
+          height:350px;
+        }
+
+
+      </style>
+
+      <div data-gjs-dmode="absolute" class="row tempHeight">
+        <div class="col">
+          <div class ='gradientBackground'/>
+        </div>
+
+        <div class="header">
+    
+          <div class="menuM">
+
+            <a class="headerItem">
+            <img src="${discord}" width="35" height="35" />
+            </a>
+            <a class="headerItem">
+              <img src="${twitter}" width="35" height="35" />
+            </a>
+            
+
+            <div class="headerItem">
+              <img src="${opensea}" width=35" height="35" />
+
+            </div>
+      
+          </div>
+        </div>
+        <div class = "nftCollectionTitleSection">
+          <div class="tripleComparisonContainer">
+           
+              <h1 class ="tripleComparisonHeader">
+                Monkey Squad
+              </h1>
+ 
+            <p class = "tripleComparisonSubHeader">
+              10,000 space travelers looking for a way home by building and creating together.
+            </p>
+           
+            </div>
+        </div>
+        
+  
+        <img class ="space1Image" src="${space1}"/>
+        <img class ="space2Image" src="${space2}"/>
+        <img class ="space3Image" src="${space3}"/>
+
+        <div class = "NFTSpecPosition">
+          <div class = "NFTSpecContainer">
+            <p class ="NFTSpecTitle"> The Specs </p>
+            <p class = "MFTSpecContent"> Each monkey is unique and programmatically generated from over 200 possible traits, including expression, headwear, clothing, and more. 
+            Some of the monkeys are more rare than others.
+            The monkeys are stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS. Mint price to be determined.
+             </p>
+          </div>
+        </div>
+
+
+
+
+        
+        <div class = "tripleComparisonSection">
+          <div class="tripleComparisonContainer">
+            <p class="tripleComparisonHeader">
+              Grow together
+            </p>
+            <p class="tripleComparisonSubHeader">
+              Build and invest as a community
+            </p>
+          </div>
+          <div class="teamContainer">
+            <div class="infoSection">
+              <div class="itemCenter">
+                <img src="${astro}" width="300" height="200" />
+                <div class="infoSectionTitle"> Build </div>
+                <p class="infoSectionDesc">
+                  ful learning rarely comes from passively consuming content. 
+                  It comes from building, teaching, a
+                </p>
+              </div>
+            </div>
+
+            <div class="infoSection">
+              <div class="itemCenter">
+                <img src="${coding}" width="300" height="200" />
+                <div class="infoSectionTitle"> Create </div>
+                <p class="infoSectionDesc">
+                  ful learning rarely comes from passively consuming content. 
+                  It comes from building, teaching, a
+                </p>
+              </div>
+            </div>
+
+            <div class="infoSection">
+              <div class="itemCenter">
+                <img src="${apartment}" width="300" height="200" />
+                <div class="infoSectionTitle"> Own </div>
+                <p class="infoSectionDesc">
+                  ful learning rarely comes from passively consuming content. 
+                  It comes from building, teaching, a
+                </p>
+              </div>
+            </div>
+
+
+
+
+            </div>
+          </div>
+        </div>
+
+
+
+
+        <div class="teamSection ">
+     
+              <div class="teamTitle">
+              Team
+              </div>
+  
+          <div class="teamContainer">
+            
+            <div class="card">
+              <img class ="testFirstPersonImage" src = "https://cdn.pixabay.com/photo/2022/01/17/17/20/bored-6945309_960_720.png"/>
+              <div class="container">
+                <h4 class="teamPersonName"><b>John Doe</b></h4>
+                <p class="teamPersonRole">Founding Artist</p>
+              </div>
+            </div> 
+
+            <div class="card">
+              <img class ="testFirstPersonImage" src = "https://cdn.pixabay.com/photo/2022/02/18/16/09/ape-7020995_960_720.png"/>
+              <div class="container">
+                <h4 class="teamPersonName"><b>Jillian Lee</b></h4>
+                <p class="teamPersonRole">Head of Marketing</p>
+              </div>
+            </div> 
+            
+           
+            <div class="card">
+              <img class ="testFirstPersonImage" src = "https://cdn.pixabay.com/photo/2022/01/06/10/10/nft-6919119_960_720.jpg"/>
+              <div class="container">
+                <h4 class="teamPersonName"><b>John Doe</b></h4>
+                <p class="teamPersonRole">Head of Community Affairs </p>
+                </div>
+            </div> 
+            
+            <div class="card">
+              <img class ="testFirstPersonImage" src = "https://cdn.pixabay.com/photo/2022/01/06/10/10/nft-6919119_960_720.jpg"/>
+              <div class="container">
+                <h4 class="teamPersonName"><b>John Doe</b></h4>
+                <p class="teamPersonRole">Head of Community Affairs </p>
+              </div>
+            </div> 
+
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    `
+  },
   {
     id: 'ConnectToWalletButton',
     label: 'Connect To Wallet Button',
